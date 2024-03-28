536ff360b47405ad337b6c3d2ac99f63dc23c8cd
shinjaesang <tlswltkd1011@gmail.com>
2024-03-28 15:14:54 +0900

    직접 풀어보기 4-2

---
+++ app/src/main/java/kr/ac/propertiestest/Direct4_2Activity.kt
@@ -0,0 +1,11 @@
+package kr.ac.propertiestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Direct4_2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direct42)
+    }
+}
